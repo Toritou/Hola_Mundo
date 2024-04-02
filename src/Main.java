b3d8acb27b5808d29a35c7de57dbac6da956e7d8
clictu <b.merino05@ufromail.cl>
2024-04-02 09:54:42 -0300

    arreglo4 se le quitó el apellido con el espacio

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hola bastian");
+        System.out.println("Hola bastian merino");
     }
 
     //hola
