fb2d5e06334a71a6279b2cce55bbc7a6a3bfd243
clictu <b.merino05@ufromail.cl>
2024-04-02 09:16:43 -0300

    arreglo4 se le quitó el apellido

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hola bastian");
+        System.out.println("Hola bastian merino");
     }
 
     //hola
